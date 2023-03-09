97ad80c8d10f980c62dbee925fc3505116e3c4bd
karen_dang <kdang@ucdavis.edu>
2023-03-09 11:09:13 -0800

    button and indicator update

--- studio2/script.js
+++ studio2/script.js
@@ -9,7 +9,21 @@
     const mainTag = document.querySelector('main');
     const next = document.querySelector('#next');
     const previous = document.querySelector('#previous');
+    
+    //indicators
+    const dot1 = document.querySelector('#slide1');
+    const dot2 = document.querySelector('#slide2');
+    const dot3 = document.querySelector('#slide3');
 
+    if (mainTag.style.right='0'){
+        previous.classList.add('hidebutton');
+    } else if (mainTag.style.right='2400px'){
+        next.classList.add('hidebutton');
+
+    }
+
+
+    
     // next button
     next.addEventListener('click', function(){
         console.log('clicked next'); 
@@ -17,10 +31,17 @@
         console.log(currentContainer);
 
         if (currentContainer > 1) {
-            if (currentContainer == 2) {
+            if (currentContainer == 2) { //second container
                 mainTag.style.right='1200px';
-            } else if (currentContainer == 3) {
+                dot1.style.backgroundColor = '#d241316f';
+                dot2.style.backgroundColor='#D24031';
+                previous.classList.remove('hidebutton');
+            } else if (currentContainer == 3){ //third container
                 mainTag.style.right='2400px';
+                dot1.style.backgroundColor = '#d241316f';
+                dot2.style.backgroundColor='#d241316f';
+                dot3.style.backgroundColor='#D24031';
+                next.classList.add('hidebutton');
             } else {
                 currentContainer = 3;
                 console.log(currentContainer);
@@ -37,10 +58,18 @@
         console.log(currentContainer);
 
 
-        if (currentContainer == 2) {
+        if (currentContainer == 2) { //second container
             mainTag.style.right='1200px';
-        } else if (currentContainer ==1) {
+            dot1.style.backgroundColor = '#d241316f';
+            dot2.style.backgroundColor='#D24031';
+            dot3.style.backgroundColor='#d241316f';
+        } else if (currentContainer ==1) { // first container
             mainTag.style.right='0';
+            dot1.style.backgroundColor = '#D24031';
+            dot2.style.backgroundColor='#d241316f';
+            dot3.style.backgroundColor='#d241316f';
+            previous.classList.add('hidebutton');
+            next.classList.remove('hidebutton');
         } else {
             currentContainer = 1;
             console.log(currentContainer);
@@ -48,6 +77,8 @@
         // mainTag.style.left='0';
     })
 
+    
+    
     // grandpa overlay
 
     document.querySelector('#grandpa2').addEventListener('click', function(event){
