d555fc8b4216cdec7cb579448c0f6e2679185db9
karen_dang <kdang@ucdavis.edu>
2023-03-11 13:13:39 -0800

    fixed files

--- final/v1/script.js
+++ final/v1/script.js
@@ -3,13 +3,12 @@
     console.log('reading js')
     
     
-
     // // slideshow of photos
     let currentContainer = 1;
     const mainTag = document.querySelector('main');
     const next = document.querySelector('#next');
     const previous = document.querySelector('#previous');
-    
+
     //indicators
     const dot1 = document.querySelector('#slide1');
     const dot2 = document.querySelector('#slide2');
@@ -23,7 +22,7 @@
     }
 
 
-    
+
     // next button
     next.addEventListener('click', function(){
         console.log('clicked next'); 
@@ -33,6 +32,7 @@
         if (currentContainer > 1) {
             if (currentContainer == 2) { //second container
                 mainTag.style.right='1200px';
+            } else if (currentContainer == 3) {
                 dot1.style.backgroundColor = '#d241316f';
                 dot2.style.backgroundColor='#D24031';
                 previous.classList.remove('hidebutton');
@@ -46,11 +46,8 @@
                 currentContainer = 3;
                 console.log(currentContainer);
             } 
-
         }
-
     })
-
     // previous button
     previous.addEventListener('click', function(){
         console.log('clicked previous');
@@ -60,10 +57,10 @@
 
         if (currentContainer == 2) { //second container
             mainTag.style.right='1200px';
+        } else if (currentContainer ==1) {
             dot1.style.backgroundColor = '#d241316f';
             dot2.style.backgroundColor='#D24031';
             dot3.style.backgroundColor='#d241316f';
-            next.classList.remove('hidebutton');
         } else if (currentContainer ==1) { // first container
             mainTag.style.right='0';
             dot1.style.backgroundColor = '#D24031';
@@ -78,153 +75,52 @@
         // mainTag.style.left='0';
     })
 
-    
-    
+
+
     // grandpa overlay
-    const picContainer1 = document.querySelector('#picContainer1');
-    const grandpa2 = document.querySelector('#grandpa2');
-
-    picContainer1.addEventListener('mouseover', fadeInGrandpa);
-    
-    function fadeInGrandpa(){
-        grandpa2.style.opacity = 1;
-    }
-
-    picContainer1.addEventListener('mouseout', fadeOutGrandpa)
-
-    function fadeOutGrandpa(){
-        grandpa2.style.opacity = 0;
-    }
 
     document.querySelector('#grandpa2').addEventListener('click', function(event){
         event.preventDefault();
         document.getElementById('coffee').className ='showing';
-        picContainer1.removeEventListener('mouseout', fadeOutGrandpa);
     })
-
     document.querySelector('#closeGrandpa').addEventListener('click', function (event) {
         event.preventDefault();
         document.getElementById('coffee').className ='hidden';
-        fadeOutGrandpa();
-        picContainer1.addEventListener('mouseout', fadeOutGrandpa);
-
     })
-
     // grandma overlay
-    const picContainer2 = document.querySelector('#picContainer2');
-    const grandma2 = document.querySelector('#grandma2');
-
-    picContainer2.addEventListener('mouseover', fadeInGrandma);
-    
-    function fadeInGrandma(){
-        grandma2.style.opacity = 1;
-    }
-
-    picContainer2.addEventListener('mouseout', fadeOutGrandma)
-
-    function fadeOutGrandma(){
-        grandma2.style.opacity = 0;
-    }
-
     document.querySelector('#grandma2').addEventListener('click', function(event){
         event.preventDefault();
         document.getElementById('flatRice').className ='showing';
-        picContainer2.removeEventListener('mouseout', fadeOutGrandma);
     })
-
     document.querySelector('#closeGrandma').addEventListener('click', function (event) {
         event.preventDefault();
         document.getElementById('flatRice').className ='hidden';
-        fadeOutGrandma();
-        picContainer2.addEventListener('mouseout', fadeOutGrandma);
-
     })
-
     // mom overlay
-    const picContainer3 = document.querySelector('#picContainer3');
-    const mom2 = document.querySelector('#mom2');
-
-    picContainer3.addEventListener('mouseover', fadeInMom);
-    
-    function fadeInMom(){
-        mom2.style.opacity = 1;
-    }
-
-    picContainer3.addEventListener('mouseout', fadeOutMom)
-
-    function fadeOutMom(){
-        mom2.style.opacity = 0;
-    }
     document.querySelector('#mom2').addEventListener('click', function(event){
         event.preventDefault();
         document.getElementById('fruit').className ='showing';
-        picContainer3.removeEventListener('mouseout', fadeOutMom);
     })
-
     document.querySelector('#closeMom').addEventListener('click', function (event) {
         event.preventDefault();
         document.getElementById('fruit').className ='hidden';
-        fadeOutMom();
-        picContainer3.addEventListener('mouseout', fadeOutMom);
-
     })
-
     // dad overlay
-    const picContainer4 = document.querySelector('#picContainer4');
-    const dad2 = document.querySelector('#dad2');
-
-    picContainer4.addEventListener('mouseover', fadeInDad);
-    
-    function fadeInDad(){
-        dad2.style.opacity = 1;
-    }
-
-    picContainer4.addEventListener('mouseout', fadeOutDad)
-
-    function fadeOutDad(){
-        dad2.style.opacity = 0;
-    }
     document.querySelector('#dad2').addEventListener('click', function(event){
         event.preventDefault();
         document.getElementById('nuggets').className ='showing';
-        picContainer4.removeEventListener('mouseout', fadeOutDad);
     })
-
     document.querySelector('#closeDad').addEventListener('click', function (event) {
         event.preventDefault();
         document.getElementById('nuggets').className ='hidden';
-        fadeOutDad();
-        picContainer4.addEventListener('mouseout', fadeOutDad);
-
     })
-
     // sister overlay
-    const picContainer5 = document.querySelector('#picContainer5');
-    const sister2 = document.querySelector('#sisterthick');
-
-    picContainer5.addEventListener('mouseover', fadeInSister);
-    
-    function fadeInSister(){
-        sister2.style.opacity = 1;
-    }
-
-    picContainer5.addEventListener('mouseout', fadeOutSister);
-
-    function fadeOutSister(){
-        sister2.style.opacity = 0;
-    }
     document.querySelector('#sisterthick').addEventListener('click', function(event){
         event.preventDefault();
         document.getElementById('mango').className ='showing';
-        picContainer5.removeEventListener('mouseout', fadeOutSister);
     })
-
     document.querySelector('#closeSister').addEventListener('click', function (event) {
         event.preventDefault();
         document.getElementById('mango').className ='hidden';
-        fadeOutSister();
-        picContainer5.addEventListener('mouseout', fadeOutSister);
-
     })
-
 })();
--- studio2/script.js
+++ studio2/script.js
@@ -1,29 +1,15 @@
 (function(){
     'use strict';
     console.log('reading js')
-    
-    
+
+
 
     // // slideshow of photos
     let currentContainer = 1;
     const mainTag = document.querySelector('main');
     const next = document.querySelector('#next');
     const previous = document.querySelector('#previous');
-    
-    //indicators
-    const dot1 = document.querySelector('#slide1');
-    const dot2 = document.querySelector('#slide2');
-    const dot3 = document.querySelector('#slide3');
 
-    if (mainTag.style.right='0'){
-        previous.classList.add('hidebutton');
-    } else if (mainTag.style.right='2400px'){
-        next.classList.add('hidebutton');
-
-    }
-
-
-    
     // next button
     next.addEventListener('click', function(){
         console.log('clicked next'); 
@@ -31,17 +17,10 @@
         console.log(currentContainer);
 
         if (currentContainer > 1) {
-            if (currentContainer == 2) { //second container
+            if (currentContainer == 2) {
                 mainTag.style.right='1200px';
-                dot1.style.backgroundColor = '#d241316f';
-                dot2.style.backgroundColor='#D24031';
-                previous.classList.remove('hidebutton');
-            } else if (currentContainer == 3){ //third container
+            } else if (currentContainer == 3) {
                 mainTag.style.right='2400px';
-                dot1.style.backgroundColor = '#d241316f';
-                dot2.style.backgroundColor='#d241316f';
-                dot3.style.backgroundColor='#D24031';
-                next.classList.add('hidebutton');
             } else {
                 currentContainer = 3;
                 console.log(currentContainer);
@@ -58,19 +37,10 @@
         console.log(currentContainer);
 
 
-        if (currentContainer == 2) { //second container
+        if (currentContainer == 2) {
             mainTag.style.right='1200px';
-            dot1.style.backgroundColor = '#d241316f';
-            dot2.style.backgroundColor='#D24031';
-            dot3.style.backgroundColor='#d241316f';
-            next.classList.remove('hidebutton');
-        } else if (currentContainer ==1) { // first container
+        } else if (currentContainer ==1) {
             mainTag.style.right='0';
-            dot1.style.backgroundColor = '#D24031';
-            dot2.style.backgroundColor='#d241316f';
-            dot3.style.backgroundColor='#d241316f';
-            previous.classList.add('hidebutton');
-            next.classList.remove('hidebutton');
         } else {
             currentContainer = 1;
             console.log(currentContainer);
@@ -78,152 +48,64 @@
         // mainTag.style.left='0';
     })
 
-    
-    
     // grandpa overlay
-    const picContainer1 = document.querySelector('#picContainer1');
-    const grandpa2 = document.querySelector('#grandpa2');
-
-    picContainer1.addEventListener('mouseover', fadeInGrandpa);
-    
-    function fadeInGrandpa(){
-        grandpa2.style.opacity = 1;
-    }
-
-    picContainer1.addEventListener('mouseout', fadeOutGrandpa)
-
-    function fadeOutGrandpa(){
-        grandpa2.style.opacity = 0;
-    }
 
     document.querySelector('#grandpa2').addEventListener('click', function(event){
         event.preventDefault();
         document.getElementById('coffee').className ='showing';
-        picContainer1.removeEventListener('mouseout', fadeOutGrandpa);
     })
 
     document.querySelector('#closeGrandpa').addEventListener('click', function (event) {
         event.preventDefault();
         document.getElementById('coffee').className ='hidden';
-        fadeOutGrandpa();
-        picContainer1.addEventListener('mouseout', fadeOutGrandpa);
 
     })
 
     // grandma overlay
-    const picContainer2 = document.querySelector('#picContainer2');
-    const grandma2 = document.querySelector('#grandma2');
-
-    picContainer2.addEventListener('mouseover', fadeInGrandma);
-    
-    function fadeInGrandma(){
-        grandma2.style.opacity = 1;
-    }
-
-    picContainer2.addEventListener('mouseout', fadeOutGrandma)
-
-    function fadeOutGrandma(){
-        grandma2.style.opacity = 0;
-    }
-
     document.querySelector('#grandma2').addEventListener('click', function(event){
         event.preventDefault();
         document.getElementById('flatRice').className ='showing';
-        picContainer2.removeEventListener('mouseout', fadeOutGrandma);
     })
 
     document.querySelector('#closeGrandma').addEventListener('click', function (event) {
         event.preventDefault();
         document.getElementById('flatRice').className ='hidden';
-        fadeOutGrandma();
-        picContainer2.addEventListener('mouseout', fadeOutGrandma);
 
     })
 
     // mom overlay
-    const picContainer3 = document.querySelector('#picContainer3');
-    const mom2 = document.querySelector('#mom2');
-
-    picContainer3.addEventListener('mouseover', fadeInMom);
-    
-    function fadeInMom(){
-        mom2.style.opacity = 1;
-    }
-
-    picContainer3.addEventListener('mouseout', fadeOutMom)
-
-    function fadeOutMom(){
-        mom2.style.opacity = 0;
-    }
     document.querySelector('#mom2').addEventListener('click', function(event){
         event.preventDefault();
         document.getElementById('fruit').className ='showing';
-        picContainer3.removeEventListener('mouseout', fadeOutMom);
     })
 
     document.querySelector('#closeMom').addEventListener('click', function (event) {
         event.preventDefault();
         document.getElementById('fruit').className ='hidden';
-        fadeOutMom();
-        picContainer3.addEventListener('mouseout', fadeOutMom);
 
     })
 
     // dad overlay
-    const picContainer4 = document.querySelector('#picContainer4');
-    const dad2 = document.querySelector('#dad2');
-
-    picContainer4.addEventListener('mouseover', fadeInDad);
-    
-    function fadeInDad(){
-        dad2.style.opacity = 1;
-    }
-
-    picContainer4.addEventListener('mouseout', fadeOutDad)
-
-    function fadeOutDad(){
-        dad2.style.opacity = 0;
-    }
     document.querySelector('#dad2').addEventListener('click', function(event){
         event.preventDefault();
         document.getElementById('nuggets').className ='showing';
-        picContainer4.removeEventListener('mouseout', fadeOutDad);
     })
 
     document.querySelector('#closeDad').addEventListener('click', function (event) {
         event.preventDefault();
         document.getElementById('nuggets').className ='hidden';
-        fadeOutDad();
-        picContainer4.addEventListener('mouseout', fadeOutDad);
 
     })
 
     // sister overlay
-    const picContainer5 = document.querySelector('#picContainer5');
-    const sister2 = document.querySelector('#sisterthick');
-
-    picContainer5.addEventListener('mouseover', fadeInSister);
-    
-    function fadeInSister(){
-        sister2.style.opacity = 1;
-    }
-
-    picContainer5.addEventListener('mouseout', fadeOutSister);
-
-    function fadeOutSister(){
-        sister2.style.opacity = 0;
-    }
     document.querySelector('#sisterthick').addEventListener('click', function(event){
         event.preventDefault();
         document.getElementById('mango').className ='showing';
-        picContainer5.removeEventListener('mouseout', fadeOutSister);
     })
 
     document.querySelector('#closeSister').addEventListener('click', function (event) {
         event.preventDefault();
         document.getElementById('mango').className ='hidden';
-        fadeOutSister();
-        picContainer5.addEventListener('mouseout', fadeOutSister);
 
     })
 
